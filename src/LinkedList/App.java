fd4fbb44b34a6cb91ef743c1f7c3e660e2f8a1a7
Andrzej Szewczuk <andrzej.szewczuk@gmail.com>
2017-05-02 21:52:06 +0200

    singly linked list done

--- src/LinkedList/App.java
+++ src/LinkedList/App.java
@@ -1,4 +1,22 @@
 package LinkedList;
 
 public class App {
+
+    public static void main(String[] args) {
+
+        List<Person> list = new LinkedList<Person>();
+        Person adam = new Person("Adam", 10);
+        Person kevin = new Person("Kevin", 15);
+
+
+        list.insert(adam);
+        list.insert(kevin);
+
+
+        System.out.println(list.size());
+
+        list.remove(adam);
+
+        list.traverseList();
+    }
 }
--- src/LinkedList/LinkedList.java
+++ src/LinkedList/LinkedList.java
@@ -38,15 +38,49 @@ public class LinkedList<T extends Comparable<T>> implements List<T> {
     @Override
     public void remove(T data) {
 
+        if( this.root == null) return;
+
+        --this.sizeOfList;
+
+        if( this.root.getData().compareTo(data) == 0 ) {
+            this.root = this.root.getNextNode();
+        } else {
+            remove(data, root, root.getNextNode());
+        }
+
+    }
+
+    private void remove(T dataToRemove, Node<T> previousNode, Node<T> actualNode) {
+
+        while(actualNode != null) {
+
+            if(actualNode.getData().compareTo(dataToRemove) == 0 ){
+                previousNode.setNextNode(actualNode.getNextNode());
+                actualNode = null;
+                return;
+            }
+
+            previousNode = actualNode;
+            actualNode = actualNode.getNextNode();
+        }
     }
 
     @Override
     public void traverseList() {
 
+        if(this.root == null) return;
+
+        Node<T> actualNode = this.root;
+
+        while(actualNode != null){
+            System.out.print(actualNode+" -> ");
+            actualNode = actualNode.getNextNode();
+        }
+
     }
 
     @Override
     public int size() {
-        return 0;
+        return this.sizeOfList;
     }
 }
