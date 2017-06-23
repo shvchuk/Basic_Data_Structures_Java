2b2c861a43ddb3a36072b4fd9562edf74406e040
Andrzej Szewczuk <andrzej.szewczuk@gmail.com>
2017-06-23 15:01:15 +0200

    huffman data compression initial commit

---
+++ src/DataHuffman/App.java
@@ -0,0 +1,4 @@
+package DataHuffman;
+
+public class App {
+}
---
+++ src/DataHuffman/HuffmanTree.java
@@ -0,0 +1,11 @@
+package DataHuffman;
+
+public class HuffmanTree implements Comparable<HuffmanTree>{
+
+    private int frequency;
+
+    @Override
+    public int compareTo(HuffmanTree o) {
+        return 0;
+    }
+}
