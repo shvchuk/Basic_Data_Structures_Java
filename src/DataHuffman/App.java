1f19163c0fe7b8b2a1339b8f169d977ce39b45a5
Andrzej Szewczuk <andrzej.szewczuk@gmail.com>
2017-06-25 17:57:43 +0200

    huffman coding done

--- src/DataHuffman/App.java
+++ src/DataHuffman/App.java
@@ -1,4 +1,17 @@
 package DataHuffman;
 
 public class App {
+
+    public static void main(String[] args) {
+
+        String text = "ABRAKADABRA";
+
+        int[] charFrequencies = new int[256];
+
+        for(char c: text.toCharArray()) ++charFrequencies[c];
+
+        HuffmanCoding code = new HuffmanCoding();
+        HuffmanTree huffmanTree = code.buildTree(charFrequencies);
+        code.printCodes(huffmanTree, new StringBuilder());
+    }
 }
