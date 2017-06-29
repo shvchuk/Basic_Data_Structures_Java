c71c90c52a985921bd80040c1865841eacd3b9e0
Andrzej Szewczuk <andrzej.szewczuk@gmail.com>
2017-06-30 00:24:51 +0200

    min heap implementation

---
+++ src/HeapNew/App.java
@@ -0,0 +1,29 @@
+package HeapNew;
+
+public class App {
+
+    public static void main(String[] args) throws Heap.HeapFullException, Heap.HeapEmptyException {
+
+        MinHeap<Integer> minHeap = new MinHeap<>(Integer.class);
+
+        minHeap.insert(9);
+        minHeap.insert(4);
+        minHeap.insert(17);
+        minHeap.printHeapArray();
+        minHeap.insert(6);
+        minHeap.printHeapArray();
+        minHeap.insert(100);
+        minHeap.insert(20);
+        minHeap.printHeapArray();
+        minHeap.insert(2);
+        minHeap.insert(1);
+        minHeap.insert(5);
+        minHeap.insert(3);
+        minHeap.printHeapArray();
+
+        minHeap.removeHighestPriority();
+        minHeap.printHeapArray();
+        minHeap.removeHighestPriority();
+        minHeap.printHeapArray();
+    }
+}
